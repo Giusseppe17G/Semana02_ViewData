2bcceddf6cfa9f6aebe3adbcfc99999dd96fd156
GIUSSEPPE <16100625@ue.edu.pe>
2022-05-18 00:39:06 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/SecurityController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Semana02_UESAN_FirstMVC.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Login()
+        {
+            ViewData["Bienvenida"] = "Iniciar sesión en el sistema";
+            ViewData["Año]"] = 2022;
+            return View();
+        }
+    }
+}
